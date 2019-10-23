9aaf41b9cff25ab19862c7391549b9cd1d5f78e9
Rod! <sixtycycles@gmail.com>
2019-10-23 09:08:19 -0700

    migrations

--- neatwork/fruit_cup/models.py
+++ neatwork/fruit_cup/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Building (models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return   f"{self.name} is the building name"
+
--- neatwork/fruit_cup/views.py
+++ neatwork/fruit_cup/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Building
 
 # Create your views here.
 
 def home_page (request):
-    data = {}
+
+    all_buildings = Building.objects.all()
+
+
+    data = {'all_buildings': all_buildings}
+
     return render (request, 'fruit_cup/home_page.html', data)
