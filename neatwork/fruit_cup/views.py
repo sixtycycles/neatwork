4bf4d155a1effd8a12bccae01919200ae422150e
Rod! <sixtycycles@gmail.com>
2019-10-23 09:51:37 -0700

    add navbar

--- neatwork/fruit_cup/views.py
+++ neatwork/fruit_cup/views.py
@@ -7,7 +7,5 @@ def home_page (request):
 
     all_buildings = Building.objects.all()
 
-
-
     data = {'all_buildings': all_buildings}
     return render (request, 'fruit_cup/home_page.html', data)
