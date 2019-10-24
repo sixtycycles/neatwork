36c267fb52f6c4794e8e931841645c6586ed454d
rockman-exe <45177933+rockman-exe@users.noreply.github.com>
2019-10-24 10:19:39 -0700

    added image upload

--- neatwork/fruit_cup/models.py
+++ neatwork/fruit_cup/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Building (models.Model):
     name = models.CharField(max_length=30)
-    photo = models.ImageField()
+    photo = models.ImageField(null=True)
 
     def __str__(self):
         return self.name
