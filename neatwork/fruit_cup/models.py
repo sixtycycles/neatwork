27e280c19c3a003860156b624fd2067e6f079f9c
rockman-exe <45177933+rockman-exe@users.noreply.github.com>
2019-10-24 09:47:07 -0700

    10 18 stuff with rod; added DB and building names

--- neatwork/fruit_cup/models.py
+++ neatwork/fruit_cup/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Building (models.Model):
     name = models.CharField(max_length=30)
+    photo = models.ImageField()
 
     def __str__(self):
         return self.name
